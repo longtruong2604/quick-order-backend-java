2d2c29dd2bce3e2678827d0f49eabbef85ddd4fc
longtruong2604 <longtruong2604@gmail.com>
2024-09-12 17:33:57 +0700

    first commit

---
+++ src/main/java/com/backend/backend_java/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.backend.backend_java.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.backend.backend_java.dto.request.UserRequestDTO;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @PostMapping("/")
+    public String addUser(@RequestBody UserRequestDTO user) {
+        return "User added";
+    }
+
+    @PutMapping("/{userId}")
+    public String updateUser(@PathVariable int userId, @RequestBody UserRequestDTO user) {
+        System.out.println("Update user");
+        return "User updated";
+    }
+
+    @PatchMapping("/{userId}")
+    public String changeUserStatus(@PathVariable int userId, @RequestParam boolean status) {
+        return "User Status changed";
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable int userId){
+        return "User deleted";
+    }
+
+    @GetMapping("/{userId}")
+    public UserRequestDTO getUser(@PathVariable int userId) {
+        return new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789");
+    }
+}
