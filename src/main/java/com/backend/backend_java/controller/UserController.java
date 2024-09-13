62a2e12abcadb03fd85b00155b9dd12a417d4677
longtruong2604 <longtruong2604@gmail.com>
2024-09-13 12:53:32 +0700

    feat: add validation

--- src/main/java/com/backend/backend_java/controller/UserController.java
+++ src/main/java/com/backend/backend_java/controller/UserController.java
@@ -1,37 +1,50 @@
 package com.backend.backend_java.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import com.backend.backend_java.dto.request.UserRequestDTO;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @PostMapping("/")
-    public String addUser(@RequestBody UserRequestDTO user) {
+    public String addUser(@Valid @RequestBody UserRequestDTO user) {
         return "User added";
     }
 
     @PutMapping("/{userId}")
-    public String updateUser(@PathVariable int userId, @RequestBody UserRequestDTO user) {
+    public String updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
         System.out.println("Update user");
         return "User updated";
     }
 
     @PatchMapping("/{userId}")
-    public String changeUserStatus(@PathVariable int userId, @RequestParam boolean status) {
+    public String updateStatus(@Min(1) @PathVariable int userId,
+                               @RequestParam boolean status) {
         return "User Status changed";
     }
 
     @DeleteMapping("/{userId}")
-    public String deleteUser(@PathVariable int userId){
+    public String deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId) {
         return "User deleted";
     }
 
     @GetMapping("/{userId}")
-    public UserRequestDTO getUser(@PathVariable int userId) {
+    public UserRequestDTO getUser(@PathVariable @Min(1) int userId) {
         return new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789");
+    }
+
+    @GetMapping("/list")
+    public List<UserRequestDTO> getAllUser(@RequestParam(defaultValue = "0", required = false) int pageNo,
+                                           @Min(10) @RequestParam(defaultValue = "20", required = false) int pageSize) {
+        return List.of(new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789"),
+                new UserRequestDTO("Leo", "Messi", "leomessi@email.com", "0123456456"));
     }
 }
