5667de51bc65fff0a84590cc0bcef2cdc01293ba
longtruong2604 <longtruong2604@gmail.com>
2024-09-20 19:18:52 +0700

    feat: update error handling

--- src/main/java/com/backend/backend_java/exception/ErrorResponse.java
+++ src/main/java/com/backend/backend_java/exception/ErrorResponse.java
@@ -1,20 +1,15 @@
 package com.backend.backend_java.exception;
 
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-
-import jakarta.validation.ConstraintViolationException;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
-import org.springframework.http.HttpStatus;
+import java.io.Serializable;
 
-@RestControllerAdvice
-public class ErrorResponse {
+@Setter
+@Getter
+public class ErrorResponse implements Serializable {
 
     private int status;
     private String error;
@@ -22,92 +17,4 @@ public class ErrorResponse {
     private String path;
     private Date timestamp;
 
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public Date getTimestamp() {
-        return timestamp;
-    }
-
-    @ExceptionHandler({ MethodArgumentNotValidException.class, ConstraintViolationException.class })
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleValidationException(MethodArgumentNotValidException e, WebRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorResponse.setError(HttpStatus.BAD_REQUEST.getReasonPhrase());
-
-        String message = e.getMessage();
-        int start = message.lastIndexOf("[");
-        int end = message.lastIndexOf("]");
-        message = message.substring(start + 1, end - 1);
-        errorResponse.setMessage(message);
-
-        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
-        errorResponse.setTimestamp(new Date());
-        return errorResponse;
-
-    }
-
-    @ExceptionHandler({ MethodArgumentTypeMismatchException.class })
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleInternalErrorException(Exception e, WebRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        errorResponse.setError(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
-        errorResponse.setTimestamp(new Date());
-
-        if (e instanceof MethodArgumentTypeMismatchException) {
-            errorResponse.setMessage("Failed to convert value of type");
-        }
-        return errorResponse;
-
-    }
-
-    @ExceptionHandler({ ResourceNotFoundException.class })
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleResourceNotFoundException(ResourceNotFoundException e, WebRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setTimestamp(new Date());
-        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
-        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setError(HttpStatus.NOT_FOUND.getReasonPhrase());
-        errorResponse.setMessage(e.getMessage());
-
-        return errorResponse;
-
-    }
 }
--- src/main/java/com/backend/backend_java/exception/GlobalExceptionHandler.java
+++ src/main/java/com/backend/backend_java/exception/GlobalExceptionHandler.java
@@ -2,39 +2,65 @@ package com.backend.backend_java.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import jakarta.validation.ConstraintViolationException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    private Date timestamp;
-    private int status;
-    private String error;
-    private String message;
-    private String path;
 
-    public GlobalExceptionHandler(Date timestamp, int status, String error, String message, String path) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.error = error;
-        this.message = message;
-        this.path = path;
+    @ExceptionHandler({ MethodArgumentNotValidException.class, ConstraintViolationException.class })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(MethodArgumentNotValidException e, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setError(HttpStatus.BAD_REQUEST.getReasonPhrase());
+
+        String message = e.getMessage();
+        int start = message.lastIndexOf("[");
+        int end = message.lastIndexOf("]");
+        message = message.substring(start + 1, end - 1);
+        errorResponse.setMessage(message);
+
+        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
+        errorResponse.setTimestamp(new Date());
+        return errorResponse;
+
     }
 
-    public Date getTimestamp() {
-        return timestamp;
+    @ExceptionHandler({ MethodArgumentTypeMismatchException.class })
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleInternalErrorException(Exception e, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorResponse.setError(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
+        errorResponse.setTimestamp(new Date());
+
+        if (e instanceof MethodArgumentTypeMismatchException) {
+            errorResponse.setMessage("Failed to convert value of type");
+        }
+        return errorResponse;
+
     }
 
-    public int getStatus() {
-        return status;
-    }
+    @ExceptionHandler({ ResourceNotFoundException.class })
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleResourceNotFoundException(ResourceNotFoundException e, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setTimestamp(new Date());
+        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setError(HttpStatus.NOT_FOUND.getReasonPhrase());
+        errorResponse.setMessage(e.getMessage());
 
-    public String getError() {
-        return error;
-    }
+        return errorResponse;
 
-    public String getMessage() {
-        return message;
     }
-
-    public String getPath() {
-        return path;
-    }
-
 }
